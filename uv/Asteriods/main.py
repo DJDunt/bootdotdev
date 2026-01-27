19fae011b76fe75d23a486d68c96e5ca202bcfea
DJDunt <davidjdunt@gmail.com>
2026-01-27 16:27:33 +0200

    player

--- uv/Asteriods/main.py
+++ uv/Asteriods/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,6 +12,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -20,10 +27,12 @@ def main():
                 return
 
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-
+        updatable.update(dt)  
 
 if __name__ == "__main__":
     main()
